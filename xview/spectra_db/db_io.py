cf9d1037c01b1f64912f2c06ec8d651c6c021cd4
dleshchev leshchev.denis@gmail.com <leshchev.denis@gmail.com>
2022-02-01 16:52:01 -0500

    fixed typo in print

--- xview/spectra_db/db_io.py
+++ xview/spectra_db/db_io.py
@@ -69,12 +69,12 @@ class ISSBlueskyMongoCatalog(BlueskyMongoCatalog):
     def validate_foil_edge(self, element, edge):
         uids = self.search_foil_data(element, edge)
         if len(uids) == 0:
-            raise Exception(f'Error: No {element} foil {edge}-edge was found in the databse')
+            raise Exception(f'Error: No {element} foil {edge}-edge was found in the database')
 
     def foil_spectrum(self, element, edge):
         all_uids = self.search_foil_data(element, edge)
         uid = all_uids[0] # latest
-        ds = db_proc[uid].primary.read()
+        ds = self[uid].primary.read()
         energy = ds['Energy'].values
         mu = ds['mu_norm'].values
         return energy, mu
