4f5da4ae05c18e6e5a14c09d48cbdf995e6ab496
dleshchev leshchev.denis@gmail.com <leshchev.denis@gmail.com>
2023-04-18 09:34:26 -0400

    uncommented iss_dev_analyzed stuff

--- xview/spectra_db/db_io.py
+++ xview/spectra_db/db_io.py
@@ -1,100 +1,100 @@
-# from bluesky_live.run_builder import RunBuilder
-#
-#
-#
-# from event_model import compose_run
-# from databroker._drivers.mongo_normalized import BlueskyMongoCatalog
-# from suitcase.mongo_normalized import Serializer
-# import time as ttime
-# import pkg_resources
-# spectrum_start_path = pkg_resources.resource_filename('xview', 'spectra_db/spectrum_start.json')
-# import json
-# import jsonschema
-# from os import listdir
-# from os.path import isfile, join, getmtime
-# import numpy as np
-# import matplotlib.pyplot as plt
-# from xas.file_io import load_interpolated_df_from_file
-#
-#
-# #metadata = {'Sample_name': 'Pt', 'compound': 'Pt', 'Element' : 'Pt', 'Edge' : 'L3', 'E0': 11564}
-# # data = {'Energy': [1, 2, 3], 'mu_norm': [0.1, 0.2, 0.3]}
-#
-#
-# def validate_schema(input_dict, schema_path):
-#     with open(schema_path) as f:
-#         contents = json.load(f)
-#     jsonschema.validate(input_dict, contents)
-#
-#
-# def generate_timestamps(keys):
-#     timestamps = {}
-#     current_time = ttime.time()
-#     for key in keys:
-#         timestamps[key] = current_time
-#     return timestamps
-#
-#
-# def _save_spectrum_to_db(serializer, metadata, data):
-#
-#     with RunBuilder(metadata=metadata) as builder:
-#     # builder = RunBuilder(metadata=metadata)
-#         run = builder.get_run()
-#         builder.add_stream(
-#             "primary",
-#             data=data
-#         )
-#     for name, doc in run.documents(fill="no"):
-#
-#         serializer(name, doc)
-#
-#     return run.metadata['start']['uid']
-#
-#
-#
-#
-# uri = "mongodb://xf08id-ca1:27017/dev_analyzed_data"
-#
-# def save_spectrum_to_db(metadata, data):
-#     ser = Serializer(uri, uri)
-#     uid = _save_spectrum_to_db(ser, metadata, data)
-#     return uid
-#
-#
-# class ISSBlueskyMongoCatalog(BlueskyMongoCatalog):
-#
-#     def search_foil_data(self, element, edge):
-#         return list(self.search({'Sample_name' : f'{element} foil', 'Edge' : edge}))
-#
-#     def validate_foil_edge(self, element, edge):
-#         uids = self.search_foil_data(element, edge)
-#         if len(uids) == 0:
-#             raise Exception(f'Error: No {element} foil {edge}-edge was found in the database')
-#
-#     def foil_spectrum(self, element, edge):
-#         all_uids = self.search_foil_data(element, edge)
-#         uid = all_uids[0] # latest
-#         ds = self[uid].primary.read()
-#         energy = ds['Energy'].values
-#         mu = ds['mu_norm'].values
-#         return energy, mu
-#
-#     def read_spectrum(self, uid):
-#         run = self[uid]
-#         data = run.primary.read()
-#         energy = data.Energy.values
-#         mu = data.mu_norm.values
-#         name = run.metadata['start']['Sample_name']
-#         return energy, mu, name
-#
-#
-#
-# def get_spectrum_catalog():
-#     return ISSBlueskyMongoCatalog(uri, uri)
-#
-#
-#
-# ######################## file/folder handling #############################
+from bluesky_live.run_builder import RunBuilder
+
+
+
+from event_model import compose_run
+from databroker._drivers.mongo_normalized import BlueskyMongoCatalog
+from suitcase.mongo_normalized import Serializer
+import time as ttime
+import pkg_resources
+spectrum_start_path = pkg_resources.resource_filename('xview', 'spectra_db/spectrum_start.json')
+import json
+import jsonschema
+from os import listdir
+from os.path import isfile, join, getmtime
+import numpy as np
+import matplotlib.pyplot as plt
+from xas.file_io import load_interpolated_df_from_file
+
+
+#metadata = {'Sample_name': 'Pt', 'compound': 'Pt', 'Element' : 'Pt', 'Edge' : 'L3', 'E0': 11564}
+# data = {'Energy': [1, 2, 3], 'mu_norm': [0.1, 0.2, 0.3]}
+
+
+def validate_schema(input_dict, schema_path):
+    with open(schema_path) as f:
+        contents = json.load(f)
+    jsonschema.validate(input_dict, contents)
+
+
+def generate_timestamps(keys):
+    timestamps = {}
+    current_time = ttime.time()
+    for key in keys:
+        timestamps[key] = current_time
+    return timestamps
+
+
+def _save_spectrum_to_db(serializer, metadata, data):
+
+    with RunBuilder(metadata=metadata) as builder:
+    # builder = RunBuilder(metadata=metadata)
+        run = builder.get_run()
+        builder.add_stream(
+            "primary",
+            data=data
+        )
+    for name, doc in run.documents(fill="no"):
+
+        serializer(name, doc)
+
+    return run.metadata['start']['uid']
+
+
+
+
+uri = "mongodb://xf08id-ca1:27017/dev_analyzed_data"
+
+def save_spectrum_to_db(metadata, data):
+    ser = Serializer(uri, uri)
+    uid = _save_spectrum_to_db(ser, metadata, data)
+    return uid
+
+
+class ISSBlueskyMongoCatalog(BlueskyMongoCatalog):
+
+    def search_foil_data(self, element, edge):
+        return list(self.search({'Sample_name' : f'{element} foil', 'Edge' : edge}))
+
+    def validate_foil_edge(self, element, edge):
+        uids = self.search_foil_data(element, edge)
+        if len(uids) == 0:
+            raise Exception(f'Error: No {element} foil {edge}-edge was found in the database')
+
+    def foil_spectrum(self, element, edge):
+        all_uids = self.search_foil_data(element, edge)
+        uid = all_uids[0] # latest
+        ds = self[uid].primary.read()
+        energy = ds['Energy'].values
+        mu = ds['mu_norm'].values
+        return energy, mu
+
+    def read_spectrum(self, uid):
+        run = self[uid]
+        data = run.primary.read()
+        energy = data.Energy.values
+        mu = data.mu_norm.values
+        name = run.metadata['start']['Sample_name']
+        return energy, mu, name
+
+
+
+def get_spectrum_catalog():
+    return ISSBlueskyMongoCatalog(uri, uri)
+
+
+
+######################### file/folder handling #############################
 #
 # def files_in_folder(folder, ext):
 #     fpaths =[join(folder, f) for f in listdir(folder) if isfile(join(folder, f))
